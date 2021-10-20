c8ab756662c9ca79c7f0cc376fc1524d47c03dcf
nithishchowdam <chnithish2001@gmail.com>
2021-10-20 19:14:46 +0530

    completed

--- Components/Filter.js
+++ Components/Filter.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {Row,Col,Button} from 'antd';
 import   '../Styles/FilterStyles.css';
-import {CaretDownOutlined } from '@ant-design/icons';
+import {CaretDownOutlined,CaretUpOutlined } from '@ant-design/icons';
 import FilterBox from './FilterBox';
 
 
 function Filter() {
+    const [alpha,changeAlpha]=useState(false)
+    const handleAlphaDesc=()=>{
+        changeAlpha(!alpha)
+    }
+    const handleAlphaAsc=()=>{
+        changeAlpha(!alpha)
+    }
     return (
         <div className="container">
             <Row>
@@ -14,7 +21,10 @@ function Filter() {
                 </Col>
                 <Col className="content" xs={24} sm={24} md={14} lg={15}>
                     <span><Button className="sortButton">Sort By A-Z</Button></span>
-                    <span><Button className="sortButton"><span><CaretDownOutlined /></span>Sort By Rating ASC</Button></span>
+                    {alpha?
+                    <span><Button className="sortButton" onClick={handleAlphaAsc}><span><CaretDownOutlined /></span>Sort By Rating ASC</Button></span>:
+                    <span><Button className="sortButton" onClick={handleAlphaDesc}><span><CaretUpOutlined /></span>Sort By Rating DESC</Button></span>
+                    }
                     <div className="contentBox">
                     </div>
                 </Col>
